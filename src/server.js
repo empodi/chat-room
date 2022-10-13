46459303bbf18fabf94ef606d8ccd5e9751905ed
empodi <kglein9513@gmail.com>
2022-10-13 19:48:31 +0900

    구현은 성공

--- src/server.js
+++ src/server.js
@@ -1,14 +1,31 @@
 import express from "express";
 
+const socketio = require("socket.io");
+const fs = require("fs");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const PORT = 4000;
 
 app.get("/", (req, res) => {
-  return res.send("SERVER!!");
+  fs.readFile("./chat.html", (error, data) => {
+    if (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+io.sockets.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    io.sockets.emit("message", data);
+  });
 });
 
 const handleListen = () => {
   console.log(`🎸 Server Listening on PORT ${PORT}`);
 };
 
-app.listen(PORT, handleListen);
+http.listen(PORT, handleListen);
